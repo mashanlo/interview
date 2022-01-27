60046b15866a6f46b22460ae8136e6db405aa693
Lee Mashanlo <52922953+mashanlo@users.noreply.github.com>
2022-01-27 20:12:08 +0600

    Add files via upload

---
+++ test-1/js/script.js
@@ -0,0 +1,35 @@
+function checkBrackets() {
+  let args = [...arguments];
+
+  args.forEach((item) => {
+    let brackets = '()[]{}';
+    let bracketsArray = [];
+
+    for (let i = 0; i < item.length; i++) {
+      let ind = brackets.indexOf(item[i]);
+      if (ind >= 0) {
+        bracketsArray.push(item[i]);
+      }
+    }
+
+    for (let i = bracketsArray.length - 1; i >= 0; i--) {
+      if (brackets.indexOf(bracketsArray[i]) % 2) {
+        if (brackets.indexOf(bracketsArray[i]) === brackets.indexOf(bracketsArray[i - 1]) + 1) {
+          bracketsArray.splice(i - 1, 2);
+        }
+      }
+    }
+
+    if (bracketsArray.length) {
+      console.log('KO');
+    }
+    else {
+      console.log('OK');
+    }
+
+  })
+}
+
+console.log(`Пример вызовова функций: checkBrackets( '(', '(hello world)' )`);
+
+checkBrackets( '(', '(hello world)' );
---
+++ test-3/js/script.js
@@ -0,0 +1,45 @@
+function getNewArr(num) {
+  let arr = String(num);
+  let odd = 0;
+  let even = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2) {
+      odd++
+    }
+    else {
+      even++;
+    }
+  }
+
+  if (odd === even) {
+    console.log(arr);
+  }
+  else if (odd > even) {
+    for (let i = 0; i < odd - even; i++) {
+      if (arr[arr.length - 1] % 2 !== 0) {
+        arr = arr.slice(0, -1);
+      }
+      else if (arr[0] % 2 !== 0) {
+        arr = arr.slice(1, arr.length);
+      }
+    }
+    console.log(arr);
+  }
+  else if (odd < even) {
+    for (let i = 0; i < even - odd; i++) {
+      if (arr[arr.length - 1] % 2 === 0) {
+        arr = arr.slice(0, -1);
+      }
+      else if (arr[0] % 2 === 0) {
+        arr = arr.slice(1, arr.length);
+      }
+    }
+    console.log(arr);
+  }
+
+}
+
+console.log('Пример вызовова функций: getNewArr(123)');
+
+getNewArr(123);
